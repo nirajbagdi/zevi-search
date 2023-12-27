9acc40ace81fc2f16539246e2c636d3b588eb51c
Niraj Bagdi <bagdiniraj22@gmail.com>
2023-12-27 15:14:34 +0530

    SCSS Initialization

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 
+import 'styles/main.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);

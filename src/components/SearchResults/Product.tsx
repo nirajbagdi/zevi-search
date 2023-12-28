f9daf6eb786767ab30afcc9fcf8740e8a329e05c
Niraj Bagdi <bagdiniraj22@gmail.com>
2023-12-28 15:09:34 +0530

    Ability to like products

--- src/components/SearchResults/Product.tsx
+++ src/components/SearchResults/Product.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import Rating from './Rating';
 
 import { ReactComponent as HeartSVG } from 'assets/heart.svg';
@@ -15,30 +18,42 @@ type Props = {
 	};
 };
 
-const Product: React.FC<Props> = ({ product }) => (
-	<article className={styles.product}>
-		<section className={styles.imgContainer}>
-			<figure>
-				<img src={require(`assets/${product.image}`)} alt={product.name} />
-				<figcaption>{product.name}</figcaption>
-			</figure>
+const Product: React.FC<Props> = ({ product }) => {
+	const [hasLiked, setHasLiked] = useState(false);
 
-			<div className={styles.actions}>
-				<button className={styles.heartBtn} aria-label="Add to favorites">
-					<HeartSVG />
-				</button>
+	const toggleHasLiked = () => setHasLiked(liked => !liked);
 
-				<button className={styles.viewBtn}>View Product</button>
+	return (
+		<article className={styles.product}>
+			<section className={styles.imgContainer}>
+				<figure>
+					<img src={require(`assets/${product.image}`)} alt={product.name} />
+					<figcaption>{product.name}</figcaption>
+				</figure>
+
+				<div className={styles.actions}>
+					<motion.button
+						whileTap={{ scale: 3 }}
+						transition={{ duration: 0.5 }}
+						className={`${styles.heartBtn} ${hasLiked ? styles.filled : ''}`}
+						aria-label="Add to favorites"
+						onClick={toggleHasLiked}
+					>
+						<HeartSVG />
+					</motion.button>
+
+					<button className={styles.viewBtn}>View Product</button>
+				</div>
+			</section>
+
+			<div className={styles.priceContainer}>
+				<span>Rs.{product.originalPrice}</span>
+				<span>Rs.{product.discountedPrice}</span>
 			</div>
-		</section>
 
-		<div className={styles.priceContainer}>
-			<span>Rs.{product.originalPrice}</span>
-			<span>Rs.{product.discountedPrice}</span>
-		</div>
-
-		<Rating rating={product.rating} totalRatings={product.ratingCount} />
-	</article>
-);
+			<Rating rating={product.rating} totalRatings={product.ratingCount} />
+		</article>
+	);
+};
 
 export default Product;

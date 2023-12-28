d1ec41fd978fb42a2d9508c03ccca8000040dd12
Niraj Bagdi <bagdiniraj22@gmail.com>
2023-12-28 17:27:42 +0530

    Reverted actions on Filters.tsx

--- src/components/SearchResults/Filters.tsx
+++ src/components/SearchResults/Filters.tsx
@@ -1,23 +1,103 @@
 import Rating from './Rating';
-import Filter from './Filter';
+
+import { ReactComponent as ArrowDown } from 'assets/arrow-down.svg';
 
 import styles from 'styles/components/SearchResults.module.scss';
 
 const Filters = () => {
 	return (
 		<section className={styles.filters}>
-			<Filter isOpen title="Brand" items={['Mango', 'H&M']} />
-			<Filter title="Price Range" items={['Under 500', '1000 to 3000']} />
-			<Filter
-				title="Ratings"
-				items={[
-					<Rating rating={5} />,
-					<Rating rating={4} />,
-					<Rating rating={3} />,
-					<Rating rating={2} />,
-					<Rating rating={1} />,
-				]}
-			/>
+			<div className={styles.filter}>
+				<header>
+					<button>
+						<p className={styles.title}>Brand</p>
+						<ArrowDown />
+					</button>
+				</header>
+
+				<ul className={styles.filterNames}>
+					<li>
+						<input type="checkbox" id="mango" />
+						<label htmlFor="mango" className={styles.label}>
+							Mango
+						</label>
+					</li>
+
+					<li>
+						<input type="checkbox" id="HM" />
+						<label htmlFor="HM" className={styles.label}>
+							H&M
+						</label>
+					</li>
+				</ul>
+			</div>
+
+			<div className={styles.filter}>
+				<header>
+					<button>
+						<p className={styles.title}>Price Range</p>
+						<ArrowDown />
+					</button>
+				</header>
+
+				<ul className={styles.filterNames}>
+					<li>
+						<input type="checkbox" id="price1" />
+						<label htmlFor="price1" className={styles.label}>
+							Under 500
+						</label>
+					</li>
+
+					<li>
+						<input type="checkbox" id="price2" />
+						<label htmlFor="price2" className={styles.label}>
+							1000 to 3000
+						</label>
+					</li>
+				</ul>
+			</div>
+
+			<div className={styles.filter}>
+				<header>
+					<button>
+						<p className={styles.title}>Ratings</p>
+						<ArrowDown />
+					</button>
+				</header>
+
+				<ul className={styles.filterNames}>
+					<li>
+						<input type="checkbox" id="5" />
+						<label htmlFor="5" className={styles.label}>
+							<Rating rating={5} />
+						</label>
+					</li>
+					<li>
+						<input type="checkbox" id="4" />
+						<label htmlFor="4" className={styles.label}>
+							<Rating rating={4} />
+						</label>
+					</li>
+					<li>
+						<input type="checkbox" id="3" />
+						<label htmlFor="3" className={styles.label}>
+							<Rating rating={3} />
+						</label>
+					</li>
+					<li>
+						<input type="checkbox" id="2" />
+						<label htmlFor="2" className={styles.label}>
+							<Rating rating={2} />
+						</label>
+					</li>
+					<li>
+						<input type="checkbox" id="1" />
+						<label htmlFor="1" className={styles.label}>
+							<Rating rating={1} />
+						</label>
+					</li>
+				</ul>
+			</div>
 		</section>
 	);
 };
